023d09c7bf3e5efc25d395898b2f9723ff42d79c
scoorf <scoorf@snowmoth.com>
2012-04-11 02:19:07 +0200

    Added ViewAsPdf.

--- Rotativa.Demo/Controllers/HomeController.cs
+++ Rotativa.Demo/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Rotativa.Demo.Controllers
 {
@@ -33,7 +28,7 @@ namespace Rotativa.Demo.Controllers
         {
             // Now I realize that this isn't very expressive example of why this can be useful.
             // However imagine that you have your own UrlHelper extensions like UrlHelper.User(...)
-            // where you create right URL according to passed conditions, prepare some complex model, etc.
+            // where you create correct URL according to passed conditions, prepare some complex model, etc.
 
             var urlHelper = new UrlHelper(Request.RequestContext);
             string url = urlHelper.Action("Index", new { name = "Giorgio II." });
@@ -47,6 +42,18 @@ namespace Rotativa.Demo.Controllers
             // You can do that by specifying full URL.
 
             return new UrlAsPdf("http://www.github.com") { FileName = "TestExternalUrl.pdf" };
+        }
+
+        public ActionResult TestView()
+        {
+            // The more usual way of using this would be to have a Model object that you would pass into ViewAsPdf
+            // and work with that Model inside your View.
+            // Good example could be an Order Summary page on some fictional E-shop.
+
+            // Probably the biggest advantage of this approach is that you have Session object available.
+
+            ViewBag.Message = string.Format("Hello {0} to ASP.NET MVC!", "Giorgio III.");
+            return new ViewAsPdf(this, "~/Views/Home/Index.cshtml", "~/Views/Shared/_Layout.cshtml", null) { FileName = "TestView.pdf" };
         }
 
         public ActionResult ErrorTest()
---
+++ Rotativa/ViewAsPdf.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rotativa
+{
+    public class ViewAsPdf : ActionResult
+    {
+        private const string ContentType = "application/pdf";
+
+        public string FileName { get; set; }
+        public string WkhtmltopdfPath { get; set; }
+
+        private readonly Controller _controller;
+        private readonly string _viewName;
+        private readonly string _masterName;
+        private readonly object _model;
+
+        public ViewAsPdf(Controller controller, string viewName, string masterName, object model)
+        {
+            WkhtmltopdfPath = string.Empty;
+
+            _controller = controller;
+            _viewName = viewName;
+            _masterName = masterName;
+            _model = model;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            var response = PrepareResponse(context.HttpContext.Response);
+
+            if (WkhtmltopdfPath == string.Empty)
+                WkhtmltopdfPath = HttpContext.Current.Server.MapPath("~/Rotativa");
+
+            _controller.ViewData.Model = _model;
+
+            using (var sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = ViewEngines.Engines.FindView(_controller.ControllerContext, _viewName, _masterName);
+                ViewContext viewContext = new ViewContext(_controller.ControllerContext, viewResult.View, _controller.ViewData, _controller.TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                StringBuilder html = sw.GetStringBuilder();
+
+                // replace href and src attributes with full URLs
+                string baseUrl = string.Format("{0}://{1}", HttpContext.Current.Request.Url.Scheme, HttpContext.Current.Request.Url.Authority);
+                html.Replace(" href=\"/", string.Format(" href=\"{0}/", baseUrl));
+                html.Replace(" src=\"/", string.Format(" src=\"{0}/", baseUrl));
+
+                var fileContent = WkhtmltopdfDriver.ConvertHtml(WkhtmltopdfPath, html.ToString());
+                response.OutputStream.Write(fileContent, 0, fileContent.Length);
+            }
+        }
+
+        private HttpResponseBase PrepareResponse(HttpResponseBase response)
+        {
+            response.ContentType = ContentType;
+
+            if (!String.IsNullOrEmpty(FileName))
+            {
+                // TODO: strip non US_ANSI chars from the filename
+                response.AddHeader("Content-Disposition", "attachment; filename=" + FileName);
+            }
+
+            response.AddHeader("Content-Type", ContentType);
+
+            return response;
+        }
+    }
+}
