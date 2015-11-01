befa6dcc718e9e5708cc40a0bda189c57ec279c8
stephen@complii.com.au <stephen@complii.com.au>
2015-11-01 23:03:56 +0800

    Added a class QueueableViewAsPdf to support generating pdf files in HostingEnvironment.QueueBackgroundWorkItem()

--- Rotativa.Demo/Models/AccountModels.cs
+++ Rotativa.Demo/Models/AccountModels.cs
@@ -23,7 +23,7 @@ namespace Rotativa.Demo.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 
@@ -61,7 +61,7 @@ namespace Rotativa.Demo.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
