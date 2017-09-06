5cd25ccf90e14c894219283ce3979cc6cc07bfb1
webgio <giorgio.bozio@gmail.com>
2017-09-06 15:31:32 +0200

    test cleanup

--- Rotativa.Tests/PdfTester.cs
+++ Rotativa.Tests/PdfTester.cs
@@ -16,22 +16,6 @@ namespace Rotativa.Tests
 
         public bool PdfIsValid { get; set; }
 
-        //public void LoadPdf(RequestResult pdfresult)
-        //{
-        //    try
-        //    {
-        //        byte[] pdfcontent = System.Text.Encoding.UTF8.GetBytes(pdfresult.ResponseText);
-        //        this.pdfReader = new iTextSharp.text.pdf.PdfReader(pdfcontent);
-        //        var parser = new PDFParser();
-        //        var parsed = parser.ExtractTextFromPDFBytes(pdfcontent);
-        //        this.PdfIsValid = true;
-        //    }
-        //    catch (iTextSharp.text.exceptions.InvalidPdfException)
-        //    {
-        //        this.PdfIsValid = false;
-        //    }
-        //}
-
         public void LoadPdf(byte[] pdfcontent)
         {
             try
@@ -47,21 +31,14 @@ namespace Rotativa.Tests
             }
         }
 
-        /// <summary>
-        /// Not really useful since wkhtmltopdf build an image pdf...
-        /// </summary>
-        /// <param name="text"></param>
-        /// <returns></returns>
         public bool PdfContains(string text)
         {
-            //var textbuilder = new StringBuilder();
             for (int page = 1; page <= pdfReader.NumberOfPages; page++)
             {
                 var strategy = new SimpleTextExtractionStrategy();
                 string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
 
                 currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
-                //textbuilder.Append(currentText);
                 if (currentText.Contains(text))
                     return true;
                 pdfReader.Close();
